33e2e16de83429719a70ac4b82e9def05915dfda
zhangjie008@sinosoft.com.cn <Ab123456#>
2021-12-27 21:06:16 +0800

    搭建环境

---
+++ clothesMemberService/src/main/java/com/example/clothesmemberservice/ClothesMemberServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.clothesmemberservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothesMemberServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClothesMemberServiceApplication.class, args);
+    }
+
+}
---
+++ clothesMemberService/src/test/java/com/example/clothesmemberservice/ClothesMemberServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clothesmemberservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothesMemberServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
